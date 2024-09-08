cab9e07bb38385701e2535498c094305227f1c70
jaeyo03 <toyazx@naver.com>
2024-09-08 22:05:21 +0900

    feat : select pose, add new canvas, draw grid

--- src/app/camera-capture/page.js
+++ src/app/camera-capture/page.js
@@ -4,7 +4,8 @@ import React, { useRef, useEffect, useState } from 'react';
 import * as tf from '@tensorflow/tfjs';
 import * as poseDetection from '@tensorflow-models/pose-detection';
 import '@tensorflow/tfjs-backend-webgl';
-import { vectorizeAndNormalize, cosineDistanceMatching, convertStructure } from './cosine_similarity';
+import { vectorizeAndNormalize, cosineDistanceMatching, convertStructure } from './cosineSimilarity';
+import PoseCombobox from './poseCombobox'
 
 const WebcamCapture = () => {
     const webcamRef = useRef(null);
@@ -13,8 +14,11 @@ const WebcamCapture = () => {
     const [startCamera, setStartCamera] = useState(false);
     const [captured, setCaptured] = useState(false);
     const [clothImage, setClothImage] = useState(null);
+    const [userImage, setUserImage] = useState(null);
     const [resultImage, setResultImage] = useState(null);
+    const [clothImageURL, setClothImageURL] = useState(null); // 옷 이미지 URL 상태 추가
     const clothImageRef = useRef(clothImage);
+    const [selectPose, setSelectPose] = useState("");
 
     const targetIndices = ['0', '2', '5', '7', '8', '11', '12', '13', '14', '15', '16', '23', '24', '25', '26'];
     let cosineScore = 0;
@@ -44,6 +48,10 @@ const WebcamCapture = () => {
         right_ear: { x: 313.4173382621787, y: 158.23918306161954 },
         left_ear: { x: 451.5738539350578, y: 156.1445775354981 }
     };
+
+    const leftArmUpPose = {'nose': {'x': 396.40524455611387, 'y': 175.09918559463986}, 'right_shoulder': {'x': 237.3481324958124, 'y': 353.90731825795643}, 'right_elbow': {'x': 194.42123943048574, 'y': 616.732994757119}, 'right_wrist': {'x': 249.53522978224458, 'y': 802.6902601340034}, 'left_shoulder': {'x': 510.4489916247906, 'y': 366.2749112227806}, 'left_elbow': {'x': 551.3259212730318, 'y': 575.6561289782245}, 'left_wrist': {'x': 476.32109665427504, 'y': 382.08359836431225}, 'right_hip': {'x': 304.1614047217538, 'y': 811.6335868465432}, 'right_knee': {'x': 298.57427118644074, 'y': 976.4748983050847}, 'left_hip': {'x': 486.6445495798319, 'y': 802.8534857142857}, 'left_knee': {'x': 502.3579892473117, 'y': 976.8301827956989}, 'right_eye': {'x': 365.86808040201004, 'y': 142.00157152428812}, 'left_eye': {'x': 429.4940502512563, 'y': 147.21057001675044}, 'right_ear': {'x': 320.59064504504505, 'y': 157.45666936936937}, 'left_ear': {'x': 465.9295121495327, 'y': 169.20981906542053}}
+
+    const rightArmUpPose = {'nose': {'x': 374.12605466237943, 'y': 175.31340868167203}, 'right_shoulder': {'x': 249.37011316211874, 'y': 368.93027929374}, 'right_elbow': {'x': 205.8747783788122, 'y': 572.6430149277688}, 'right_wrist': {'x': 300.8984471153846, 'y': 386.8471669230769}, 'left_shoulder': {'x': 523.0291460674158, 'y': 351.77373033707863}, 'left_elbow': {'x': 566.7686592295346, 'y': 616.7623467094703}, 'left_wrist': {'x': 502.30193258426965, 'y': 792.8664959871589}, 'right_hip': {'x': 276.31714255663434, 'y': 807.7223236245954}, 'right_knee': {'x': 265.65123275862067, 'y': 972.2458333333335}, 'left_hip': {'x': 456.2124667747164, 'y': 816.0725316045381}, 'left_knee': {'x': 463.5282643678161, 'y': 981.4974770114942}, 'right_eye': {'x': 341.09564790996785, 'y': 145.18524667202573}, 'left_eye': {'x': 405.628962962963, 'y': 143.15544711755234}, 'right_ear': {'x': 301.2527380530974, 'y': 165.412140460177}, 'left_ear': {'x': 446.2283657657658, 'y': 160.36092836036036}}
 
     useEffect(() => {
         const loadModel = async () => {
@@ -105,25 +113,20 @@ const WebcamCapture = () => {
                     const userPoseVector = vectorizeAndNormalize(userPose, allBody);
                     const targetPoseVector = vectorizeAndNormalize(targetPose, allBody);
                     cosineScore = 1 - cosineDistanceMatching(userPoseVector,targetPoseVector);
-                    drawCanvas(pose, video, video.videoWidth, video.videoHeight, canvasRef);
+
+
+                    drawCanvas(selectPose, video, video.videoWidth, video.videoHeight, canvasRef);
 
                     // 캡쳐 조건 추가
                     setWholeBodyScore(cosineScore);
                     setLeftBodyScore(leftCosineScore);
                     setRightBodyScore(rightCosineScore);
-
-                    // if (cosineScore >= 0.9){
-                    //     if (!captured){
-                    //         setCaptured(true);
-                    //         captureAndSendImage();
-                    //     }
-                    // }
                 }
             }
         };
         const interval = setInterval(detectPose, 100);
         return () => clearInterval(interval);
-    }, [detector, captured]);
+    }, [detector, selectPose]);
 
     const drawCanvas = (pose, video, videoWidth, videoHeight, canvas) => {
         const ctx = canvas.current.getContext('2d');
@@ -133,21 +136,31 @@ const WebcamCapture = () => {
         ctx.clearRect(0, 0, videoWidth, videoHeight);
         ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
 
-        if (pose) {
-            targetIndices.forEach(index => {
-                const keypoint = pose.keypoints[index];
+        let selectedPose = null;
+
+        if (pose === "armsDown"){
+            selectedPose = targetPose;
+        } else if(pose === "leftArm"){
+            selectedPose = leftArmUpPose;
+        } else if(pose === "rightArm"){
+            selectedPose = rightArmUpPose;
+        }
+
+        console.log(pose,selectedPose)
+
+        if (selectedPose) {
+            Object.keys(pose).forEach(bodyKey => {
+                const keypoint = pose[bodyKey]
                 if (keypoint) {
-                    const { x, y, score } = keypoint;
-                    if (score > 0.8){
-                        ctx.beginPath();
-                        ctx.arc(x, y, 5, 0, 2 * Math.PI);
-                        if (cosineScore > 0.9){
-                            ctx.fillStyle = 'green';
-                        } else {
-                            ctx.fillStyle = 'red';
-                        }
-                        ctx.fill();
+                    const { x, y } = keypoint;
+                    ctx.beginPath();
+                    ctx.arc(x, y, 5, 0, 2 * Math.PI);
+                    if (cosineScore > 0.9){
+                        ctx.fillStyle = 'green';
+                    } else {
+                        ctx.fillStyle = 'red';
                     }
+                    ctx.fill();
                 }
             });
         }
@@ -156,9 +169,10 @@ const WebcamCapture = () => {
     useEffect(() => {
         clothImageRef.current = clothImage;  // clothImage 상태가 변경될 때마다 ref 업데이트
     }, [clothImage]);
-
+    
+    // 점수가 높을때 캡쳐 시행
     useEffect(() => {
-        if (wholeBodyScore >= 0.9) {
+        if (wholeBodyScore >= 0.95) {
             if (!captured) {
                 setCaptured(true);
                 captureAndSendImage();
@@ -178,17 +192,24 @@ const WebcamCapture = () => {
         }
 
         alert('Start Tryon!')
+
+        // 원본 비디오 프레임 캡처를 위한 새로운 캔버스 생성
+        const tempCanvas = document.createElement('canvas');
+        const tempCtx = tempCanvas.getContext('2d');
+
+        tempCanvas.width = canvasRef.current.width;
+        tempCanvas.height = canvasRef.current.height;
+
+        // 비디오 프레임을 새 캔버스에 그립니다.
+        tempCtx.drawImage(webcamRef.current, 0, 0, tempCanvas.width, tempCanvas.height);
+
+        // 2024.09.07 - 원래 쓰이던 canvas , 아래에 tempCanvas를 canvas로 바꿔주면 됨
         const canvas = canvasRef.current;
 
-        // 아무것도 안 그려진것을 보내기 위한 코드
-        const ctx = canvas.getContext('2d');
-        // 캔버스 클리어 (이전에 그려진 모든 요소 삭제)
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        // 비디오 프레임을 캔버스에 그립니다.
-        ctx.drawImage(webcamRef.current, 0, 0, canvas.width, canvas.height);
+        const vtonImage = tempCanvas.toDataURL('image/png');  // 이미지 데이터 URL 생성
 
-        const vtonImage = canvas.toDataURL('image/png');  // 이미지 데이터 URL 생성
+        setUserImage(vtonImage);
 
         // 이미지 전송을 위해 Data URL을 Blob으로 변환
         // 추후 변수명 바꾸는게 좋을듯
@@ -223,12 +244,15 @@ const WebcamCapture = () => {
     }
 
     const handleClothImageChange = (e) => {
-        setClothImage(e.target.files[0]);
+        const file = e.target.files[0];
+        setClothImage(file);
+        setClothImageURL(URL.createObjectURL(file));
     };
 
     return (
         <div align={'center'}>
             <input type="file" accept="image/*" onChange={handleClothImageChange} />
+            <PoseCombobox value={selectPose} setValue={setSelectPose}/>
             <video
                 ref={webcamRef}
                 style={{ display: 'none' }}
@@ -273,9 +297,19 @@ const WebcamCapture = () => {
                 </div>
             </div>
             {resultImage && (
-                <div>
-                    <h2>Result Image</h2>
-                    <img src={resultImage} alt="Virtual Try-On Result"/>
+                <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '20px'}}>
+                    <div style={{margin: '10px'}}>
+                        <h3>Cloth Image</h3>
+                        <img src={clothImageURL} alt="Cloth Image" style={{width: '500px'}}/>
+                    </div>
+                    <div style={{margin: '10px'}}>
+                        <h3>User Image</h3>
+                        <img src={userImage} alt="User Image" style={{width: '500px'}}/>
+                    </div>
+                    <div style={{margin: '10px'}}>
+                        <h3>Result Image</h3>
+                        <img src={resultImage} alt="Virtual Try-On Result" style={{width: '500px'}}/>
+                    </div>
                 </div>
             )}
         </div>
