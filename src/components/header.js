532a7c5185eb9364700acd38c4a70593964b20bb
jaeyo03 <toyazx@naver.com>
2024-09-10 00:12:22 +0900

    feat : change logo

--- src/components/header.js
+++ src/components/header.js
@@ -18,9 +18,9 @@ export default function Header() {
                     className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6" href="/">
                     <div
                         className="flex flex-none items-center justify-center border border-neutral-200 bg-white dark:border-neutral-700 dark:bg-black h-[40px] w-[40px] rounded-xl">
-                        <svg xmlns="http://www.w3.org/2000/svg" aria-label="Acme Store logo" viewBox="0 0 32 28"
+                        <svg xmlns="http://www.w3.org/2000/svg" aria-label="Acme Store logo" viewBox="0 0 32 32"
                              className="h-4 w-4 fill-black dark:fill-white h-[16px] w-[16px]">
-                            <path d="M21.5758 9.75769L16 0L0 28H11.6255L21.5758 9.75769Z"></path>
+                            <path d="M4 0H28V4H8V12H24V16H8V28H4V0Z"></path>
                             <path d="M26.2381 17.9167L20.7382 28H32L26.2381 17.9167Z"></path>
                         </svg>
                     </div>
@@ -29,16 +29,7 @@ export default function Header() {
                     <ul className="hidden gap-6 text-sm md:flex md:items-center">
                         <li><a
                             className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
-                            href="/search/bags">Bags</a></li>
-                        <li><a
-                            className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
                             href="/search/clothing">Clothing</a></li>
-                        <li><a
-                            className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
-                            href="/search/shop-all">Shop All</a></li>
-                        <li><a
-                            className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
-                            href="/search/stickers">Stickers</a></li>
                     </ul>
                 </div>
                 <div className="hidden justify-center md:flex md:w-1/3">
@@ -57,14 +48,14 @@ export default function Header() {
                     </form>
                 </div>
                 <div className="flex justify-end md:w-1/3">
-                    <button aria-label="Open cart">
+                    <button aria-label="Open User Info">
                         <div
                             className="relative flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                  stroke="currentColor" aria-hidden="true"
                                  className="h-4 transition-all ease-in-out hover:scale-110 ">
                                 <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"></path>
+                                      d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path>
                             </svg>
                         </div>
                     </button>
