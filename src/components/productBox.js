510fdbec8f21383528004f26f1cd8055b5525451
jaeyo03 <toyazx@naver.com>
2024-09-21 00:34:07 +0900

    feat : add product price

--- src/components/productBox.js
+++ src/components/productBox.js
@@ -26,10 +26,13 @@ const ProductBox = ({ product }) => {
                     <div className="absolute bottom-0 left-0 flex w-full px-4 pb-4 @container/label">
                         <div
                             className="flex items-center rounded-full border bg-white/70 p-1 text-xs font-semibold text-black backdrop-blur-md dark:border-neutral-800 dark:bg-black/70 dark:text-white">
-                            <h3 className="mr-4 line-clamp-2 flex-grow pl-2 leading-none tracking-tight">Acme
-                                Rainbow Sticker</h3><p
-                            className="flex-none rounded-full bg-blue-600 p-2 text-white">$4.00<span
-                            className="ml-1 inline hidden @[275px]/label:inline">USD</span></p></div>
+                            <h3 className="mr-4 line-clamp-2 flex-grow pl-2 leading-none tracking-tight">
+                                {product.name}
+                            </h3>
+                            <p className="flex-none rounded-full bg-blue-600 p-2 text-white">
+                                {product.price}₩
+                            </p>
+                        </div>
                     </div>
                 </div>
             </Link>
