afda540289a42d953afd53aeaac746a3d5740c52
jaeyo03 <toyazx@naver.com>
2024-09-21 00:34:46 +0900

    feat : make new menu nav

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import UserMenu from "@/components/userMenu";
+import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
+import {Button} from "@/components/ui/button";
+import {Home, LineChart, Package, Package2, PanelLeft, ShoppingCart, Users2} from "lucide-react";
 
 export default function Header() {
 
@@ -8,13 +11,59 @@ export default function Header() {
         <nav className="relative flex items-center justify-between p-4 lg:px-6">
             {/*모바일 화면에서 나오는 메뉴바*/}
             <div className="block flex-none md:hidden">
-                <button aria-label="Open mobile menu"
-                        className="flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white md:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                         stroke="currentColor" aria-hidden="true" className="h-4">
-                        <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
-                    </svg>
-                </button>
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button size="icon" variant="outline" className="sm:hidden">
+                            <PanelLeft className="h-5 w-5" />
+                            <span className="sr-only">Toggle Menu</span>
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent side="left" className="sm:max-w-xs">
+                        <nav className="grid gap-6 text-lg font-medium">
+                            <Link
+                                href="/"
+                                className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
+                            >
+                                <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
+                            </Link>
+                            <Link
+                                href="/"
+                                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                            >
+                                <Home className="h-5 w-5" />
+                                Dashboard
+                            </Link>
+                            <Link
+                                href="/"
+                                className="flex items-center gap-4 px-2.5 text-foreground"
+                            >
+                                <ShoppingCart className="h-5 w-5" />
+                                Orders
+                            </Link>
+                            <Link
+                                href="#"
+                                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                            >
+                                <Package className="h-5 w-5" />
+                                Products
+                            </Link>
+                            <Link
+                                href="#"
+                                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                            >
+                                <Users2 className="h-5 w-5" />
+                                Customers
+                            </Link>
+                            <Link
+                                href="#"
+                                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                            >
+                                <LineChart className="h-5 w-5" />
+                                Settings
+                            </Link>
+                        </nav>
+                    </SheetContent>
+                </Sheet>
             </div>
             <div className="flex w-full items-center">
                 <div className="flex w-full md:w-1/3"><a
@@ -32,8 +81,16 @@ export default function Header() {
                     <ul className="hidden gap-6 text-sm md:flex md:items-center">
                         <li>
                             <a
-                            className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
-                            href="/search/clothing">Clothing</a></li>
+                                className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
+                                href="/search/clothing">
+                                Clothing
+                            </a>
+                            <a
+                                className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300 m-6"
+                                href="/dashboard">
+                                MyPage
+                            </a>
+                        </li>
                     </ul>
                 </div>
                 <div className="hidden justify-center md:flex md:w-1/3">
@@ -43,10 +100,8 @@ export default function Header() {
                                                                                              className="w-full rounded-lg border bg-white px-4 py-2 text-sm text-black placeholder:text-neutral-500 dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400"
                                                                                              name="search" value=""/>
                         <div className="absolute right-0 top-0 mr-3 flex h-full items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                 stroke="currentColor" aria-hidden="true" className="h-4">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" className="h-4">
+                                <path d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
                             </svg>
                         </div>
                     </form>
