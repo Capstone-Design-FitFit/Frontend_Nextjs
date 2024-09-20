38ae4365c97e959e973cc6d5ee70430b4734949b
jaeyo03 <toyazx@naver.com>
2024-09-21 00:34:22 +0900

    feat : make new menu nav

--- src/components/userMenu.js
+++ src/components/userMenu.js
@@ -1,22 +1,41 @@
 import {
-    Sheet, SheetClose,
-    SheetContent,
-    SheetDescription,
-    SheetFooter,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger
-} from "@/components/ui/sheet";
-import {Button} from "@/components/ui/button";
-import {Label} from "@/components/ui/label";
-import {Input} from "@/components/ui/input";
-import Image from "next/image";
+    Cloud,
+    CreditCard,
+    Keyboard,
+    LifeBuoy,
+    LogOut,
+    Mail,
+    MessageSquare,
+    Plus,
+    PlusCircle,
+    Settings,
+    User,
+    UserPlus,
+    Users,
+} from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+
 
 const UserMenu = () => {
     return (
-        <Sheet>
-            <SheetTrigger asChild>
-                {/*유저 버튼*/}
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
                 <Button variant="outline">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                          stroke="currentColor" aria-hidden="true"
@@ -24,47 +43,84 @@ const UserMenu = () => {
                         <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path>
                     </svg>
                 </Button>
-            </SheetTrigger>
-            <SheetContent className="overflow-y-auto">
-                <SheetHeader>
-                    <SheetTitle>Hello! 진수</SheetTitle>
-                    <SheetDescription>
-                        Add new poses of you! We will check if it appropriate for try-on.
-                    </SheetDescription>
-                </SheetHeader>
-                <div className="grid gap-4 py-4">
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="name" className="text-right">
-                            Name
-                        </Label>
-                        <Input id="User ID" value="Pedro Duarte" className="col-span-3"/>
-                    </div>
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="username" className="text-right">
-                            Username
-                        </Label>
-                        <Input id="username" value="@peduarte" className="col-span-3"/>
-                    </div>
-                </div>
-                <SheetFooter className="mt-4">
-                    <Button type="submit">Upload Pose</Button>
-                </SheetFooter>
-                <div className="grid gap-4 py-4">
-                    <Image
-                        src="/images/t-shirt.jpg"
-                        width={400}
-                        height={300}
-                        alt="picture of t-shirt"
-                    />
-                    <Image
-                        src="/images/t-shirt.jpg"
-                        width={400}
-                        height={300}
-                        alt="picture of t-shirt"
-                    />
-                </div>
-            </SheetContent>
-        </Sheet>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                        <User className="mr-2 h-4 w-4" />
+                        <span>Profile</span>
+                        <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <CreditCard className="mr-2 h-4 w-4" />
+                        <span>Billing</span>
+                        <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <Settings className="mr-2 h-4 w-4" />
+                        <span>Settings</span>
+                        <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <Keyboard className="mr-2 h-4 w-4" />
+                        <span>Keyboard shortcuts</span>
+                        <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                        <Users className="mr-2 h-4 w-4" />
+                        <span>Team</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuSub>
+                        <DropdownMenuSubTrigger>
+                            <UserPlus className="mr-2 h-4 w-4" />
+                            <span>Invite users</span>
+                        </DropdownMenuSubTrigger>
+                        <DropdownMenuPortal>
+                            <DropdownMenuSubContent>
+                                <DropdownMenuItem>
+                                    <Mail className="mr-2 h-4 w-4" />
+                                    <span>Email</span>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <MessageSquare className="mr-2 h-4 w-4" />
+                                    <span>Message</span>
+                                </DropdownMenuItem>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>
+                                    <PlusCircle className="mr-2 h-4 w-4" />
+                                    <span>More...</span>
+                                </DropdownMenuItem>
+                            </DropdownMenuSubContent>
+                        </DropdownMenuPortal>
+                    </DropdownMenuSub>
+                    <DropdownMenuItem>
+                        <Plus className="mr-2 h-4 w-4" />
+                        <span>New Team</span>
+                        <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                    <LifeBuoy className="mr-2 h-4 w-4" />
+                    <span>Support</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem disabled>
+                    <Cloud className="mr-2 h-4 w-4" />
+                    <span>API</span>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                    <LogOut className="mr-2 h-4 w-4" />
+                    <span>Log out</span>
+                    <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
     )
 }
 
