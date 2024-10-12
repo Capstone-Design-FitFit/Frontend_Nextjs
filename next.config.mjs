c03bb674e7985e6c43dfa294f479994e0bb11926
jaeyo03 <toyazx@naver.com>
2024-10-12 22:38:59 +0900

    feat : s3 domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['photo-clothing-result-s3.s3.amazonaws.com'], // S3 버킷 도메인 추가
+    },
+};
 
 export default nextConfig;
