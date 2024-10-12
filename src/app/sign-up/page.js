cb0a612b1a126e0931d4196d8a2af6ab50da2ead
jaeyo03 <toyazx@naver.com>
2024-10-12 22:40:52 +0900

    feat : add auth context

--- src/app/sign-up/page.js
+++ src/app/sign-up/page.js
@@ -11,105 +11,55 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export const description =
     "A sign up form with first name, last name, email and password inside a card. There's an option to sign up with GitHub and a link to login if you already have an account";
 
 export default function SignUpForm() {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
+    const [userName,setUserName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [verifyCode, setVerifyCode] = useState("");
-    const [isVerifying, setIsVerifying] = useState(false);
+    const router = useRouter();
 
-    const test = async () => {
+    const signUp = async () => {
         const bodyData = {
-            ClientId: process.env.NEXT_PUBLIC_CLIENT_ID,
-            Username: email,
-            Password: password,
-            UserAttributes: [
-                {
-                    Name: "name",
-                    Value: firstName + " " + lastName
-                },
-                {
-                    Name: "email",
-                    Value: email
-                }
-            ]
+            username: userName,
+            password: password,
+            email: email,
         };
 
-        if (firstName.length === 0 || lastName.length === 0 || email.length === 0 || password.length === 0) {
-            alert("정보 미입력")
+        if (userName.length === 0 || email.length === 0 || password.length === 0) {
+            alert("Enter All Information!")
             return
         }
 
+        console.log(bodyData);
+
         try {
-            const response = await fetch(process.env.NEXT_PUBLIC_COGNITO_API_URL, {
-                method: "POST",
+            const response = await fetch(`${process.env.NEXT_PUBLIC_SPRING_API_URL}register`, {
+                method: 'POST',
                 headers : {
-                    "Content-Type": "application/x-amz-json-1.1",
-                    "X-Amz-Target" : "AWSCognitoIdentityProviderService.SignUp",
+                    'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(bodyData),
             });
 
-            const data = await response.json();
-            console.log(data)
+            const data = await response.text();
+
             if (response.ok) {
-                alert("인증코드가 이메일로 전송!");
-                setIsVerifying(true);
+                alert("회원가입 완료");
+                router.push("/login");
             } else {
-                alert("백엔드 오류");
+                alert(data);
             }
         } catch (error) {
-            alert("Error SignUp", error);
-        }
-    }
-
-    const test2 = async () => {
-        const bodyData2 = {
-            ClientId: process.env.NEXT_PUBLIC_CLIENT_ID,
-            Username: email,
-            ConfirmationCode : verifyCode,
-        };
-
-        if (!isVerifying) {
-            alert("First get a Verification Code");
-            return;
-        }
-
-        if (verifyCode.length === 0) {
-            alert("No Verification Code");
-            return;
-        }
-
-        try {
-            const response = await fetch(process.env.NEXT_PUBLIC_COGNITO_API_URL, {
-                method: "POST",
-                headers : {
-                    "Content-Type": "application/x-amz-json-1.1",
-                    "X-Amz-Target" : "AWSCognitoIdentityProviderService.ConfirmSignUp",
-                },
-                body: JSON.stringify(bodyData2),
-            });
-
-            const data = await response.json();
-            console.log(data)
-            if (data === {}) {
-                alert("로그인 완료");
-            } else {
-                alert("백엔드 오류");
-            }
-        } catch (error) {
-            console.log(data);
-            alert("Error SignUp", error);
+            alert(error);
         }
     }
 
     return (
-        <Card className="mx-auto max-w-sm">
+        <Card className="mx-auto max-w-sm mb-4">
             <CardHeader>
                 <CardTitle className="text-xl">Sign Up</CardTitle>
                 <CardDescription>
@@ -118,25 +68,15 @@ export default function SignUpForm() {
             </CardHeader>
             <CardContent>
                 <form className="grid gap-4">
-                    <div className="grid grid-cols-2 gap-4">
+                    <div className="gap-4">
                         <div className="grid gap-2">
-                            <Label htmlFor="first-name">First name</Label>
+                            <Label htmlFor="first-name">User Name</Label>
                             <Input
-                                id="first-name"
-                                placeholder="Max"
+                                id="username"
+                                placeholder="Jinsoo"
                                 required
-                                value={firstName}
-                                onChange={(e) => setFirstName(e.target.value)}
-                            />
-                        </div>
-                        <div className="grid gap-2">
-                            <Label htmlFor="last-name">Last name</Label>
-                            <Input
-                                id="last-name"
-                                placeholder="Robinson"
-                                required
-                                value={lastName}
-                                onChange={(e) => setLastName(e.target.value)}
+                                value={userName}
+                                onChange={(e) => setUserName(e.target.value)}
                             />
                         </div>
                     </div>
@@ -161,23 +101,9 @@ export default function SignUpForm() {
                             onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
-                    <div className="grid gap-2">
-                        <Label htmlFor="password">Verification Code</Label>
-                        <Input
-                            id="verification-code"
-                            type="verification_code"
-                            required
-                            value={verifyCode}
-                            onChange={(e) => setVerifyCode(e.target.value)}
-                        />
-                    </div>
-                    <Button variant="outline" className="w-full" onClick={test}>
-                        Send Code
-                    </Button>
-                    <Button type="submit" className="w-full" onClick={test2}>
+                    <Button type="button" className="w-full" onClick={signUp}>
                         Create an account
                     </Button>
-
                 </form>
                 <div className="mt-4 text-center text-sm">
                     Already have an account?{" "}
