692499ad836aa5f8eef7fa7ce6c6d33122163d05
jaeyo03 <toyazx@naver.com>
2024-10-14 18:56:10 +0900

    design : fix login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -63,11 +63,11 @@ export default function Login() {
     return (
         <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
             <div className="flex items-center justify-center py-12">
-                <div className="mx-auto grid w-[400px] h-[600px] gap-6">
+                <div className="mx-auto grid w-[350px] h-[600px] gap-6">
                     <div className="grid gap-2 text-center">
                         <h1 className="text-3xl font-bold">Login</h1>
                         <p className="text-balance text-muted-foreground text-2xl">
-                            Enter your email below to login to your account
+                            Enter your ID below
                         </p>
                     </div>
                     <div className="grid gap-4">
