42a9b8dae6f99e2b5fd669b23bf848bc1b46a3a1
jaeyo03 <toyazx@naver.com>
2024-10-14 18:56:47 +0900

    build : make build config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
         domains: ['photo-clothing-result-s3.s3.amazonaws.com'], // S3 버킷 도메인 추가
     },
+    output:'standalone',
 };
 
 export default nextConfig;
