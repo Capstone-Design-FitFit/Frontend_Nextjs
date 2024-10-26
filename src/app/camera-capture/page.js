4efca9f5d4e8134818dd24ecab67dea4a87f9a97
jaeyo03 <toyazx@naver.com>
2024-10-27 01:52:20 +0900

    refactor : rename

--- src/app/camera-capture/page.js
+++ src/app/camera-capture/page.js
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 
 // 서버사이드 렌더링을 비활성화한 상태로 WebcamCapture 컴포넌트 동적 로드
-const WebcamCapture = dynamic(() => import('@/app/camera-capture/webcamCapture'), {
+const WebcamCapture = dynamic(() => import('@/app/camera-capture/WebcamCapture'), {
     ssr: false,
 });
 
