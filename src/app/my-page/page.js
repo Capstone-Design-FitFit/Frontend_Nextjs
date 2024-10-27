3402f4637b202383f0bb28db195ba27cef284350
jaeyo03 <toyazx@naver.com>
2024-10-28 01:52:08 +0900

    refactor : change tags

--- src/app/my-page/page.js
+++ src/app/my-page/page.js
@@ -117,8 +117,8 @@ export default function MyPage() {
     return (
         <div className="flex w-full flex-col bg-muted/40">
             {user ? (
-            <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-                <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+            <main className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+                <div className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
                     <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-1">
                         <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1">
                             <Card x-chunk="dashboard-05-chunk-1">
@@ -182,84 +182,11 @@ export default function MyPage() {
                                         <CarouselNext className="absolute right-2"/>
                                     </Carousel>
                                 </CardContent>
-                                {/*<CardContent>*/}
-                                {/*    <Carousel opts={{align:"start"}} className="w-full">*/}
-                                {/*        <CarouselContent className="gap-4 flex items-center"> /!* 각 항목 사이에 간격 추가 *!/*/}
-                                {/*            {tryOnPhotos.map((tryonPhoto) => (*/}
-                                {/*                <>*/}
-                                {/*                    <CarouselItem key={tryonPhoto.fittingId} className="sm:basis-full md:basis-1/4 lg:basis-1/6">*/}
-                                {/*                        <div className="flex flex-col items-center"> /!* 이미지들 간의 간격을 추가 *!/*/}
-                                {/*                            <Image*/}
-                                {/*                                src={tryonPhoto.fittingImageUrl}*/}
-                                {/*                                width={200}*/}
-                                {/*                                height={200}*/}
-                                {/*                                alt="Fitting Result"*/}
-                                {/*                                className="rounded-md shadow-md"*/}
-                                {/*                            />*/}
-                                {/*                        </div>*/}
-                                {/*                    </CarouselItem>*/}
-                                {/*                    <CarouselItem key={tryonPhoto.fittingId} className="sm:basis-full md:basis-1/4 lg:basis-1/6">*/}
-                                {/*                        <div className="flex flex-col items-center">*/}
-                                {/*                        </div>*/}
-                                {/*                    </CarouselItem>*/}
-                                {/*                    <CarouselItem key={tryonPhoto.fittingId} className="sm:basis-full md:basis-1/4 lg:basis-1/6">*/}
-                                {/*                        <div className="flex flex-col items-center">*/}
-                                {/*                        </div>*/}
-                                {/*                    </CarouselItem>*/}
-                                {/*                </>*/}
-                                {/*            ))}*/}
-                                {/*        </CarouselContent>*/}
-                                {/*        <CarouselPrevious className="absolute left-2"/>*/}
-                                {/*        <CarouselNext className="absolute right-2"/>*/}
-                                {/*    </Carousel>*/}
-                                {/*</CardContent>*/}
-                            </Card>
-                            <Card x-chunk="dashboard-05-chunk-1">
-                                <CardHeader className="pb-2">
-                                    <CardTitle className="text-3xl">Poses</CardTitle>
-                                    <CardDescription>Poses that you uploaded or shoot</CardDescription>
-                                </CardHeader>
-                                <CardContent>
-                                    <Carousel opts={{align:"start"}} className="w-full">
-                                        <CarouselContent className="gap-4"> {/* 여기서 gap을 추가 */}
-                                            {photos.map((photo, index) => (
-                                                <CarouselItem key={photo.photoId} className="md:basis-1/4 lg:basis-1/6">
-                                                    <div className="flex flex-col items-center space-y-2">
-                                                        <Image src={photo.photoUrl} width={200} height={200} alt="" className="rounded-md shadow-md" />
-                                                    </div>
-                                                </CarouselItem>
-                                            ))}
-                                        </CarouselContent>
-                                        <CarouselPrevious className="absolute left-2"/>
-                                        <CarouselNext className="absolute right-2"/>
-                                    </Carousel>
-                                </CardContent>
-                            </Card>
-                            <Card x-chunk="dashboard-05-chunk-1">
-                                <CardHeader className="pb-2">
-                                    <CardTitle className="text-3xl">Clohtes</CardTitle>
-                                    <CardDescription>Clothes that you uploaded</CardDescription>
-                                </CardHeader>
-                                <CardContent>
-                                    <Carousel opts={{align:"start"}} className="w-full">
-                                        <CarouselContent className="gap-4"> {/* 여기서 gap을 추가 */}
-                                            {photos.map((photo, index) => (
-                                                <CarouselItem key={photo.photoId} className="md:basis-1/4 lg:basis-1/6">
-                                                    <div className="flex flex-col items-center space-y-2">
-                                                        <Image src={photo.photoUrl} width={200} height={200} alt="" className="rounded-md shadow-md" />
-                                                    </div>
-                                                </CarouselItem>
-                                            ))}
-                                        </CarouselContent>
-                                        <CarouselPrevious className="absolute left-2"/>
-                                        <CarouselNext className="absolute right-2"/>
-                                    </Carousel>
-                                </CardContent>
                             </Card>
                         </div>
                     </div>
-                </main>
-            </div>
+                </div>
+            </main>
         ) : (
             <div className="flex flex-col items-center justify-center h-full py-8">
                 <h2 className="text-2xl font-bold mb-4">로그인해야 사용할 수 있습니다</h2>
