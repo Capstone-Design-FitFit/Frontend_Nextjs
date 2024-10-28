d1300df966cdc28d8fbf362ba69d407afa711368
jaeyo03 <toyazx@naver.com>
2024-10-28 18:49:59 +0900

    style : fix css

--- src/app/camera-capture/ScoreCanvas.js
+++ src/app/camera-capture/ScoreCanvas.js
@@ -5,38 +5,54 @@ import React from "react";
 export default function ScoreCanvas({wholeBodyScore,leftBodyScore,rightBodyScore}) {
 
     return (
-        <div id="scoreOverlay" style={{position: 'relative', width: '768px', height: '100px'}}>
-            <canvas style={{width: '768px', height: '100px'}}/>
-            <div id="totalScore" style={{
-                position: 'absolute',
-                top: '10px',
-                left: '10px',
-                color: 'white',
-                backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                padding: '5px',
-                borderRadius: '5px'
-            }}>Total Score: {wholeBodyScore.toFixed(2)}
+        <div id="scoreOverlay" className="relative w-full h-full">
+            <canvas className="absolute top-0 left-0 w-full h-full"/>
+            <div
+                id="totalScore"
+                style={{
+                    position: 'absolute',
+                    top: '10px',
+                    left: '10px',
+                    color: 'white',
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                    padding: '5px',
+                    borderRadius: '5px',
+                    zIndex: 10,
+                }}
+            >
+                Total Score: {wholeBodyScore.toFixed(2)}
             </div>
-            <div id="leftScore" style={{
-                position: 'absolute',
-                top: '40px',
-                left: '10px',
-                color: 'white',
-                backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                padding: '5px',
-                borderRadius: '5px'
-            }}>Left Score: {leftBodyScore.toFixed(2)}
+            <div
+                id="leftScore"
+                style={{
+                    position: 'absolute',
+                    top: '40px',
+                    left: '10px',
+                    color: 'white',
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                    padding: '5px',
+                    borderRadius: '5px',
+                    zIndex: 10,
+                }}
+            >
+                Left Score: {leftBodyScore.toFixed(2)}
             </div>
-            <div id="rightScore" style={{
-                position: 'absolute',
-                top: '70px',
-                left: '10px',
-                color: 'white',
-                backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                padding: '5px',
-                borderRadius: '5px'
-            }}>Right Score: {rightBodyScore.toFixed(2)}
+            <div
+                id="rightScore"
+                style={{
+                    position: 'absolute',
+                    top: '70px',
+                    left: '10px',
+                    color: 'white',
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                    padding: '5px',
+                    borderRadius: '5px',
+                    zIndex: 10,
+                }}
+            >
+                Right Score: {rightBodyScore.toFixed(2)}
             </div>
         </div>
+
     )
 }
