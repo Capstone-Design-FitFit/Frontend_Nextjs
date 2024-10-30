805c53bbddac75b4ac0476a66fa3ca25b2d5332c
jaeyo03 <toyazx@naver.com>
2024-10-31 00:47:06 +0900

    feat : add data.js

---
+++ src/app/data.js
@@ -0,0 +1,97 @@
+export const one = {
+    name: '1',
+    displayName: 'Cloth One',
+    imageUrl: '/images/1.jpg',
+    price: '36,000',
+};
+
+export const two = {
+    name: '2',
+    displayName: 'Cloth Two',
+    imageUrl: '/images/2.jpg',
+    price: '36,000',
+};
+
+export const three = {
+    name: '3',
+    displayName: 'Cloth Three',
+    imageUrl: '/images/3.jpg',
+    price: '36,000',
+};
+
+export const four = {
+    name: '4',
+    displayName: 'Cloth Four',
+    imageUrl: '/images/4.jpg',
+    price: '36,000',
+};
+
+export const five = {
+    name: '5',
+    displayName: 'Cloth Five',
+    imageUrl: '/images/5.jpg',
+    price: '36,000',
+};
+
+export const six = {
+    name: '6',
+    displayName: 'Cloth Six',
+    imageUrl: '/images/6.jpg',
+    price: '36,000',
+};
+
+export const seven = {
+    name: '7',
+    displayName: 'Cloth Seven',
+    imageUrl: '/images/7.jpg',
+    price: '36,000',
+};
+
+export const eight = {
+    name: '8',
+    displayName: 'Cloth Eight',
+    imageUrl: '/images/8.jpg',
+    price: '36,000',
+};
+
+export const nine = {
+    name: '9',
+    displayName: 'Cloth Nine',
+    imageUrl: '/images/9.jpg',
+    price: '36,000',
+};
+
+export const ten = {
+    name: '10',
+    displayName: 'Cloth Ten',
+    imageUrl: '/images/10.jpg',
+    price: '36,000',
+};
+
+export const eleven = {
+    name: '11',
+    displayName: 'Cloth Eleven',
+    imageUrl: '/images/11.jpg',
+    price: '36,000',
+};
+
+export const twelve = {
+    name: '12',
+    displayName: 'Cloth Twelve',
+    imageUrl: '/images/12.jpg',
+    price: '36,000',
+};
+
+export const thirteen = {
+    name: '13',
+    displayName: 'Cloth Thirteen',
+    imageUrl: '/images/13.jpg',
+    price: '36,000',
+};
+
+export const fourteen = {
+    name: '14',
+    displayName: 'Cloth Fourteen',
+    imageUrl: '/images/14.jpg',
+    price: '36,000',
+};
