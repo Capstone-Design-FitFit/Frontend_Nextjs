648b93e0e013f7eb62a61307d6f66860a368a14f
jaeyo03 <toyazx@naver.com>
2024-10-31 20:52:56 +0900

    feat : add selected image

--- src/app/camera-capture/ChoosePose.js
+++ src/app/camera-capture/ChoosePose.js
@@ -19,17 +19,15 @@ export default function ChoosePose (){
     const [resultImageURL, setResultImageURL] = useState("");
     const [startTryOn, setStartTryOn] = useState(false);
     const [tryOnComplete, setTryOnComplete] = useState(false);
-    const [selectedId, setSelectedId] = useState(0);
     const searchParams = useSearchParams();
     const clothId = searchParams.get('clothId');
     const clothImage = `/images/${clothId}.jpg`;
     const [api, setApi] = useState(null);
     const [current, setCurrent] = React.useState(0)
     const [count, setCount] = React.useState(0)
-    const imgRef = useRef(null);
-    // const selectedImage = poseImageList.find(image =>
-    //     image.id === selectedId
-    // );
+    const selectedImage = poseImageList.find(image =>
+        image.photoId === current
+    );
     let resultJSON;
 
     useEffect(() => {
@@ -88,67 +86,70 @@ export default function ChoosePose (){
         }
     }
 
+    const testing = async () => {
+        setStartTryOn(true);
+        const photoUrl = selectedImage.photoUrl
+        console.log(photoUrl);
+        const match = photoUrl.match(/(\d{4}\d+)_pose_capture/);
+        console.log(match[1])
+        try{
+            const res = await fetch(photoUrl);
+            const userBlob = await res.blob();
+            console.log(userBlob);
+            setStartTryOn(false);
+        } catch (e){
+            alert(e);
+            setStartTryOn(false);
+        }
+    }
+
     const handleStartTryOn = async () => {
-        imgRef.current.crossOrigin = "anonymous";
-        const canvas = document.createElement("canvas");
-        canvas.width = imgRef.current.width;
-        canvas.height = imgRef.current.height;
+        setStartTryOn(true);
+        const photoUrl = selectedImage.photoUrl
+        const res = await fetch(photoUrl);
+        const userBlob = await res.blob();
+        console.log(userBlob);
+        const formData = new FormData();
+        const match = photoUrl.match(/(\d{4}\d+)_pose_capture/);
 
-        const ctx = canvas.getContext("2d");
-        ctx.drawImage(imgRef.current, 0, 0);
+        try {
+            // 옷 이미지 가져오기
+            const response = await fetch(clothImage, {
+                method: 'GET',
+            });
 
-        // const res = await fetch("https://photo-clothing-result-s3.s3.amazonaws.com/photo/1730292481558_pose_capture.png");
-        // const blob = await res.blob();
-        // console.log(blob);
-
-        // canvas toBlob 비동기 콜백 내에서 formData 생성 및 처리
-        canvas.toBlob(async (userBlob) => {
-            if (!userBlob) {
-                alert('이미지 캡처 실패');
-                setStartTryOn(false);
-                return;
+            if (!response.ok) {
+                throw new Error('옷 이미지를 불러오는데 실패했습니다.');
             }
 
-            const formData = new FormData();
+            const clothBlob = await response.blob();
+            const timestamp = Date.now();
 
-            try {
-                // 옷 이미지 가져오기
-                const response = await fetch(clothImage, {
-                    method: 'GET',
-                });
+            // formData에 blob 파일 추가
+            formData.append('garm_img', clothBlob, `${timestamp}_clothes.png`);
+            formData.append('vton_img', userBlob, `${match[1]}_pose_capture.png`);
+            formData.append('user_id', '1');
 
-                if (!response.ok) {
-                    throw new Error('옷 이미지를 불러오는데 실패했습니다.');
-                }
+            // 가상 피팅 API 호출
+            const tryOnResponse = await fetch(`${process.env.NEXT_PUBLIC_FAST_API_URL}/viton`, {
+                method: 'POST',
+                body: formData,
+            });
 
-                const clothBlob = await response.blob();
-                const timestamp = Date.now();
-
-                // formData에 blob 파일 추가
-                formData.append('garm_img', clothBlob, `${timestamp}_clothes.png`);
-                formData.append('vton_img', userBlob, `${timestamp}_pose_capture.png`);
-                formData.append('user_id', '1');
-
-                // 가상 피팅 API 호출
-                const tryOnResponse = await fetch(`${process.env.NEXT_PUBLIC_FAST_API_URL}/viton`, {
-                    method: 'POST',
-                    body: formData,
-                });
-
-                if (tryOnResponse.ok) {
-                    const data = await tryOnResponse.json();
-                    alert('가상 피팅 완료');
-                    resultJSON = data;
-                    await saveImage(); // saveImage 호출
-                } else {
-                    alert('가상 피팅 실패');
-                    setStartTryOn(false);
-                }
-            } catch (error) {
-                alert(`에러 발생: ${error.message}`);
+            if (tryOnResponse.ok) {
+                const data = await tryOnResponse.json();
+                alert('가상 피팅 완료');
+                resultJSON = data;
+                await saveImage(); // saveImage 호출
+            } else {
+                alert('가상 피팅 실패');
                 setStartTryOn(false);
             }
-        }, "image/png");
+        } catch (error) {
+            alert(`에러 발생: ${error.message}`);
+            setStartTryOn(false);
+        }
+
     };
 
 
@@ -174,16 +175,11 @@ export default function ChoosePose (){
                                         <Card>
                                             <CardContent className="flex aspect-square items-center justify-center p-6">
                                                 <img
-                                                    ref={img.photoId === current ? imgRef : null}
                                                     key={img.photoId}
                                                     src={img.photoUrl}
                                                     alt={`Pose ${img.photoId}`}
                                                     style={{width: '300px'}}
                                                     className="rounded-md shadow-md"
-                                                    onClick={() => {
-                                                        setSelectedId(img.photoId);
-                                                        console.log(img.photoId);
-                                                    }}
                                                 />
                                             </CardContent>
                                         </Card>
@@ -199,7 +195,7 @@ export default function ChoosePose (){
                     <div className="py-2 text-center text-sm text-muted-foreground">
                         Image {current} of {count}
                     </div>
-                    <Button onClick={handleStartTryOn}>Start TryOn!</Button>
+                    <Button onClick={testing}>Start TryOn!</Button>
                 </>
             )}
         </>
